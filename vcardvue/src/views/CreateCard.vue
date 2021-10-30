11eb56649047427a1803ea59bb3707e3a11df775
iogrt <duarte.dd.dias@gmail.com>
2021-10-30 18:41:30 +0100

    start vue project

---
+++ vcardvue/src/views/CreateCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h3>Create a vCard</h3>
+    <form class="createcard--form">
+      <div><input name="phone" type="tel" placeholder="Phone Number"></div>
+      <div><input name="password" type="password" placeholder="Password"></div>
+      <div><input type="submit" value="Create card"></div>
+    </form>
+  </div>
+</template>
---
+++ vcardvue/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="p-5 rounded-3 bg-primary text-light text-right">
+      <h1>vCard App</h1>
+      <p>Create virtual cards online</p>
+      <router-link :to="{name: 'CardCreate'}" class="btn btn-light">Create a card</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
