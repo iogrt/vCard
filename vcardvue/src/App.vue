8b3a44f31ad93642a7dc2b3ba6f73c65e6edd40c
iogrt <39342422+iogrt@users.noreply.github.com>
2021-11-02 16:11:36 +0000

    start vue project (#5)
    Co-authored-by: iogrt <duarte.dd.dias@gmail.com>

---
+++ vcardvue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Navbar/>
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+</style>
