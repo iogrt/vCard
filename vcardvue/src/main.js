33ed4631f64f8f2d02b14dde772b4ba4d46fcaa7
iogrt <duarte.dd.dias@gmail.com>
2021-11-13 16:21:34 +0000

    start of vue admin login

--- vcardvue/src/main.js
+++ vcardvue/src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:80/api'
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ vcardvue/src/views/AdminLogin.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container">
+  <div class="card p-5">
+    <h3 class="card-title text-center">Admin Login</h3>
+    <form class="card-body thin-form" @submit.prevent="tryLogin">
+      <div>
+        <input class="form-control" name="email" type="text" placeholder="E-mail"/>
+      </div>
+      <div>
+        <input class="form-control mb-3" name="password" placeholder="Password" type="pasword"/>
+      </div>
+      <div>
+        <input class="btn btn-primary mx-auto d-block" type="submit" value="Login"/>
+      </div>
+    </form>
+  </div>
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'AdminLogin',
+  methods: {
+    tryLogin () {
+      axios.get('users').then(r => console.log(r.data))
+    }
+  }
+}
+</script>
+<style>
+  .thin-form {
+  max-width: 300px;
+  margin: auto;
+  }
+</style>
