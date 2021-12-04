debaa66caa8ebc17f40d2276584436c2f7b23623
MarusDod <markitos.2001@hotmail.com>
2021-12-04 17:51:49 +0000

    user view

--- vcardlaravel/database/migrations/2021_11_06_191959_add_auth_user_view.php
+++ vcardlaravel/database/migrations/2021_11_06_191959_add_auth_user_view.php
@@ -15,7 +15,7 @@ class AddAuthUserView extends Migration
   public function up()
   {
     DB::statement("
-      CREATE VIEW view_auth_users AS
+      CREATE OR REPLACE VIEW view_auth_users AS
         SELECT phone_number as id, 'V' as user_type, phone_number as username, password, name, email, blocked, confirmation_code, photo_url, deleted_at from vcards
         union
         SELECT id, 'A', email, password, name, email, 0, null, null, null from users
