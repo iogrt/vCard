79ead769a64f5d48b0d4628a0e3d503f6fcf0dc8
Ana Cassia <anacassia.10@hotmail.com>
2021-12-07 17:40:23 +0000

    components "empty" TransactionsDetail

---
+++ vcardvue/src/components/transactions/Transaction.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h1>Edit</h1>
+    <TransactionDetail>
+    </TransactionDetail>
+</div>
+</template>
+
+<script>
+import TransactionDetail from './TransactionDetail.vue'
+
+export default {
+  name: 'Transaction',
+  components: {
+    TransactionDetail
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vcardvue/src/components/transactions/TransactionDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3 class="mt-5 mb-3">detail#</h3>
+</template>
+
+<script>
+export default {
+  name: 'TransactionDetail'
+}
+</script>
+
+<style scoped>
+
+</style>
